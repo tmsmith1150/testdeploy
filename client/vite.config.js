59401db9b98098647adc7a90d3d23a10d3888c77
Michelle Jordan <tmsmith1150@live.com>
2024-07-01 15:55:00 -0400

    deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,14 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/graphql': {
-        target: 'http://localhost:3001',
-        secure: false,
-        changeOrigin: true,
-      }
-    }
+    host: true,
+    strictPort: true,
+    port: 8000,
   }
 })
